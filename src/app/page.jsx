0f122e92f683594cf6b42c3bcce897aa1b276434
Martin <martin.dinahet@proton.me>
2024-11-23 15:54:22 +0100

    initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <div className="w-screen min-h-screen grid place-items-center">
+        <h1 className="font-bold text-2xl">Hello, World!</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
